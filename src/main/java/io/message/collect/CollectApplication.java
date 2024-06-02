ee50bc927905b66c9e9fc2989369946ad618cfbe
bellman66 <rndso15@naver.com>
2024-06-02 14:26:59 +0900

    Feat: Spring 초기 설정 추가

---
+++ src/main/java/io/message/collect/CollectApplication.java
@@ -0,0 +1,13 @@
+package io.message.collect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectApplication.class, args);
+	}
+
+}
