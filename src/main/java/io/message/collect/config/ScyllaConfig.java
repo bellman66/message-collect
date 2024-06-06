00a3626a5cdeaf3b828a488de8bf72d369edabbc
bellman66 <rndso15@naver.com>
2024-06-06 13:42:17 +0900

    Feat: Scylla 설정 완료

--- src/main/java/io/message/collect/config/ScyllaConfig.java
+++ src/main/java/io/message/collect/config/ScyllaConfig.java
@@ -1,22 +1,21 @@
 package io.message.collect.config;
 
-import java.net.InetSocketAddress;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
-import org.springframework.data.cassandra.config.SessionBuilderConfigurer;
+import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
 @AllArgsConstructor
-@EnableCassandraRepositories
 public class ScyllaConfig extends AbstractCassandraConfiguration {
 
-    private final CassandraProperties properties;
+    private CassandraProperties properties;
+
+    private static final String BASE_PACKAGE = "io.message.collect.application.data.entity";
 
     @Override
     protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
@@ -29,15 +28,33 @@ public class ScyllaConfig extends AbstractCassandraConfiguration {
     }
 
     @Override
-    protected SessionBuilderConfigurer getSessionBuilderConfigurer() {
-        return builder -> builder
-                .withKeyspace(getKeyspaceName())
-                .addContactPoint(InetSocketAddress.createUnresolved(String.valueOf(properties.getContactPoints()), properties.getPort()));
-    }
-
-    @Override
     protected String getKeyspaceName() {
         return properties.getKeyspaceName();
     }
 
+    @Override
+    public String getContactPoints() {
+        return properties.getContactPoints().getFirst();
+    }
+
+    @Override
+    protected int getPort() {
+        return properties.getPort();
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.valueOf(properties.getSchemaAction().toUpperCase());
+    }
+
+    @Override
+    protected String getLocalDataCenter() {
+        return properties.getLocalDatacenter();
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[] {BASE_PACKAGE};
+    }
+    
 }
