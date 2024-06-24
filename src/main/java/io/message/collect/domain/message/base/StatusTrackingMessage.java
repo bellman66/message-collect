ad7275bb4d7f029e40f6aec4581342f158c390dd
bellman66 <rndso15@naver.com>
2024-06-24 11:12:58 +0900

    Message Status Update 로직 추가 및 Send 로직 추가
    Feat: Message Status Update 로직 추가 및 Send 로직 추가

--- src/main/java/io/message/collect/domain/message/base/StatusTrackingMessage.java
+++ src/main/java/io/message/collect/domain/message/base/StatusTrackingMessage.java
@@ -14,4 +14,7 @@ public abstract class StatusTrackingMessage<T> extends DefaultMessage<T> {
 
     private MessageStatus status;
 
+    public void setStatus(MessageStatus status) {
+        this.status = status;
+    }
 }
--- src/main/java/io/message/collect/framework/message/KafkaConsumer.java
+++ src/main/java/io/message/collect/framework/message/KafkaConsumer.java
@@ -2,11 +2,13 @@ package io.message.collect.framework.message;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.message.collect.application.output.MessageOutput;
 import io.message.collect.application.output.SignalOutput;
 import io.message.collect.domain.message.SignalMessage;
 import io.message.collect.domain.model.MechanicalSignal;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -14,11 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.ExecutionException;
 
+import static io.message.collect.domain.enums.MessageStatus.SENT;
+
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
+    private final MessageOutput messageOutput;
     private final SignalOutput<MechanicalSignal> signalOutput;
+    private final ElasticsearchOperations elasticsearchOperations;
 
     private final ObjectMapper objectMapper;
 
@@ -26,7 +32,15 @@ public class KafkaConsumer {
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void consumePublishMessage(ConsumerRecord<String, String> record) throws ExecutionException, InterruptedException, JsonProcessingException {
         SignalMessage signalMessage = objectMapper.readValue(record.value(), SignalMessage.class);
+
+        // Save Search Database
+        elasticsearchOperations.save(signalMessage);
+
+        // Save Main Database
         signalOutput.save(signalMessage);
+
+        signalMessage.setStatus(SENT);
+        messageOutput.save(() -> signalMessage);
     }
 
 }
