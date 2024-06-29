2e0786ee0d8b53eee05a8665b115297ba6181e64
bellman66 <rndso15@naver.com>
2024-06-29 15:51:14 +0900

    elasticsearch infra 업데이트
    Fix: elasticsearch SSL 설정 업데이트

--- src/main/java/io/message/collect/config/ElasticsearchConfig.java
+++ src/main/java/io/message/collect/config/ElasticsearchConfig.java
@@ -1,30 +1,46 @@
 package io.message.collect.config;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import javax.net.ssl.SSLContext;
 import lombok.AllArgsConstructor;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchClients;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-
-import java.util.List;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @AllArgsConstructor
+@EnableElasticsearchRepositories(basePackages = "io.message.collect.domain.message")
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
     private final ElasticsearchProperties elasticsearchProperties;
 
     @Override
     public ClientConfiguration clientConfiguration() {
-        List<String> uris = elasticsearchProperties.getUris();
         return ClientConfiguration.builder()
-                .connectedTo(uris.getFirst())
-                .withClientConfigurer(ElasticsearchClients.ElasticsearchRestClientConfigurationCallback.from(restClientBuilder -> {
-                    // configure the Elasticsearch RestClient
-                    return restClientBuilder;
-                }))
+                .connectedTo(elasticsearchProperties.getUris().toArray(new String[0]))
+                .usingSsl(disableSslVerification())
+                .withBasicAuth(elasticsearchProperties.getUsername(), elasticsearchProperties.getPassword())
+                .withConnectTimeout(Duration.ofSeconds(5))
+                .withSocketTimeout(Duration.ofSeconds(3))
                 .build();
     }
 
+    public static SSLContext disableSslVerification() {
+        try {
+            return SSLContextBuilder.create()
+                    .setProtocol("SSL")
+                    .loadTrustMaterial(TrustAllStrategy.INSTANCE)
+                    .build();
+        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
