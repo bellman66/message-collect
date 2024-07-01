bcec3ab0b94fbd0d6245179cb4bb10525bd1db7f
bellman66 <rndso15@naver.com>
2024-07-01 10:58:40 +0900

    Elasticsearch Read Output 설정
    Feat: 엘라스틱서치 Output 로직 추가

--- src/main/java/io/message/collect/application/output/SignalOutput.java
+++ src/main/java/io/message/collect/application/output/SignalOutput.java
@@ -3,10 +3,13 @@ package io.message.collect.application.output;
 import io.message.collect.domain.interfaces.EntityAble;
 import io.message.collect.domain.model.MechanicalSignal;
 
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 public interface SignalOutput<T extends MechanicalSignal> {
 
     T save(EntityAble<T> messageAble) throws ExecutionException, InterruptedException;
+
+    Optional<T> findById(String id);
 
 }
--- src/main/java/io/message/collect/framework/database/SignalAdapter.java
+++ src/main/java/io/message/collect/framework/database/SignalAdapter.java
@@ -5,9 +5,11 @@ import io.message.collect.domain.interfaces.EntityAble;
 import io.message.collect.domain.model.MechanicalSignal;
 import io.message.collect.framework.database.repository.SignalRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -17,10 +19,17 @@ public class SignalAdapter implements SignalOutput<MechanicalSignal> {
 
     private final SignalRepository signalRepository;
 
+    private final ElasticsearchOperations elasticsearchOperations;
+
     @Override
     @Transactional
     public MechanicalSignal save(EntityAble<MechanicalSignal> entityAble) throws ExecutionException, InterruptedException {
         return signalRepository.save(entityAble.toEntity());
+    }
+
+    @Override
+    public Optional<MechanicalSignal> findById(String id) {
+        return Optional.ofNullable(elasticsearchOperations.get(id, MechanicalSignal.class));
     }
 
 }
