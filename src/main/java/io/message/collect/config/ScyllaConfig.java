120c47007fa491d5d65d8c06d70a9e0824c09ab9
bellman66 <rndso15@naver.com>
2024-07-05 10:56:01 +0900

    Signal 메세지 엘라스틱 설정 수정
    Refector:Signal 메세지 엘라스틱 설정 수정

--- src/main/java/io/message/collect/config/ScyllaConfig.java
+++ src/main/java/io/message/collect/config/ScyllaConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.convert.CassandraConverter;
+import org.springframework.data.cassandra.core.convert.MappingCassandraConverter;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
 
@@ -15,7 +17,7 @@ import java.util.List;
 public class ScyllaConfig extends AbstractCassandraConfiguration {
 
     private static final String BASE_PACKAGE = "io.message.collect.domain";
-    
+
     private CassandraProperties properties;
 
     @Override
@@ -58,4 +60,9 @@ public class ScyllaConfig extends AbstractCassandraConfiguration {
         return new String[]{BASE_PACKAGE};
     }
 
+    @Override
+    public CassandraConverter cassandraConverter() {
+        return new MappingCassandraConverter();
+    }
+    
 }
