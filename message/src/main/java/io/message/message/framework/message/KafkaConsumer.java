77974418496b8f690e496ed787d0079a24f76e4a
bellman66 <rndso15@naver.com>
2024-08-06 10:04:41 +0900

    kafka consumer 로직 비동기 전환
    Refector: kafka consumer 로직 비동기 전환

--- message/src/main/java/io/message/message/framework/message/KafkaConsumer.java
+++ message/src/main/java/io/message/message/framework/message/KafkaConsumer.java
@@ -4,38 +4,59 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.message.message.application.port.output.SearchOutput;
 import io.message.message.application.port.output.SignalOutput;
-import io.message.message.domain.mapper.MessageMapper;
 import io.message.message.domain.message.PendingMessage;
 import io.message.message.domain.message.SignalMessage;
 import io.message.message.domain.model.MechanicalSignal;
 import io.message.message.domain.search.SignalSearch;
-import java.util.concurrent.ExecutionException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class KafkaConsumer {
+public class KafkaConsumer implements ApplicationRunner {
 
     private final ObjectMapper objectMapper;
     private final SignalOutput<MechanicalSignal> signalOutput;
     private final SearchOutput<SignalSearch> searchOutput;
     private final KafkaProducer<PendingMessage> pendingMessageKafkaProducer;
 
-    @KafkaListener(topics = "${consumers.topics.publish-message}", groupId = "${consumers.group.name}")
-    @Transactional
-    public void consumePublishMessage(ConsumerRecord<String, String> record) throws JsonProcessingException, ExecutionException, InterruptedException {
-        SignalMessage signalMessage = objectMapper.readValue(record.value(), SignalMessage.class);
+    private final ReactiveKafkaConsumerTemplate<String, ConsumerRecord<String, String>> recordReactiveKafkaConsumerTemplate;
 
-        Mono<SignalSearch> searchSaveMono = searchOutput.save(signalMessage);
-        Mono<MechanicalSignal> signalSaveMono = signalOutput.save(signalMessage);
+    public Flux<?> consumePublishMessage() {
+        return recordReactiveKafkaConsumerTemplate
+                .receiveAutoAck()
+                .flatMap(consumerRecord -> Mono.fromCallable(() -> {
+                            try {
+                                ConsumerRecord<String, String> recordValue = consumerRecord.value();
+                                return objectMapper.readValue(recordValue.value(), SignalMessage.class);
+                            } catch (JsonProcessingException e) {
+                                return Mono.error(e);
+                            }
+                        })
+                )
+                .flatMap(signalMessage -> {
+                    try {
+                        SignalMessage signalMessage1 = (SignalMessage) signalMessage;
+                        Mono<SignalSearch> searchSaveMono = searchOutput.save(signalMessage1);
+                        Mono<MechanicalSignal> signalSaveMono = signalOutput.save(signalMessage1);
 
-        Mono.when(searchSaveMono, signalSaveMono)
-                .doOnError(throwable -> pendingMessageKafkaProducer.pending(MessageMapper.INSTANCE.toPendingMessage(signalMessage, throwable)))
-                .subscribe();
+                        return Mono.zip(searchSaveMono, signalSaveMono);
+                    } catch (Exception e) {
+                        return Mono.error(e);
+                    }
+                });
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        consumePublishMessage().subscribe();
     }
 }
