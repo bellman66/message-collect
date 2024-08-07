43e7512a7a8925851f858018d9442cd7c274bc4a
bellman66 <rndso15@naver.com>
2024-08-07 11:05:46 +0900

    kafka consumer 리엑터 설정완료 및 오류 수정
    Refector: kafka consumer 리엑터 설정완료
    Remove: 설정내 오류 수정

--- message/src/main/java/io/message/message/config/KafkaConfig.java
+++ message/src/main/java/io/message/message/config/KafkaConfig.java
@@ -1,49 +1,41 @@
 package io.message.message.config;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.ssl.DefaultSslBundleRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.sender.SenderOptions;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Configuration
 @EnableKafka
+@RequiredArgsConstructor
 public class KafkaConfig {
 
-    @Value(value = "${spring.kafka.producer.bootstrap-servers}")
-    private String BOOTSTRAPSERVERS;
+    private final KafkaProperties kafkaProperties;
+
+    @Value("${consumers.topics.publish-message}")
+    private String publishMessageTopic;
+
+    @Value("${consumers.topics.pending-message}")
+    private String pendingMessageTopic;
 
     @Bean
     public ReceiverOptions<String, String> receiverOptions() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAPSERVERS);
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "publish_message");
-        config.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
-        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return ReceiverOptions.create(config);
+        ReceiverOptions<String, String> receiverOptions = ReceiverOptions.create(kafkaProperties.buildConsumerProperties(new DefaultSslBundleRegistry()));
+        return receiverOptions.subscription(List.of(publishMessageTopic, pendingMessageTopic));
     }
 
     @Bean
     public SenderOptions<String, String> senderOptions() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAPSERVERS);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return SenderOptions.create(config);
+        return SenderOptions.create(kafkaProperties.buildProducerProperties(new DefaultSslBundleRegistry()));
     }
 
     @Bean
