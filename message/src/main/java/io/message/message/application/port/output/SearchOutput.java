5a4fb5c15367488c03c3ceaae944e3e6096f75d9
bellman66 <rndso15@naver.com>
2024-08-16 21:58:59 +0900

    불필요한 Exception throw 제거
    Refector: 불필요한 Exception throw 제거

--- message/src/main/java/io/message/message/application/port/output/SearchOutput.java
+++ message/src/main/java/io/message/message/application/port/output/SearchOutput.java
@@ -3,7 +3,6 @@ package io.message.message.application.port.output;
 import io.message.message.domain.interfaces.SearchAble;
 import io.message.message.domain.search.SignalSearch;
 import io.message.message.domain.search.base.Search;
-import java.util.concurrent.ExecutionException;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.query.Query;
 import reactor.core.publisher.Flux;
@@ -11,7 +10,7 @@ import reactor.core.publisher.Mono;
 
 public interface SearchOutput<T extends Search> {
 
-    Mono<T> save(SearchAble<T> search) throws ExecutionException, InterruptedException;
+    Mono<T> save(SearchAble<T> search);
 
     Flux<SearchHit<SignalSearch>> searchByQuery(Query query);
 }
--- message/src/main/java/io/message/message/application/port/output/SignalOutput.java
+++ message/src/main/java/io/message/message/application/port/output/SignalOutput.java
@@ -2,10 +2,9 @@ package io.message.message.application.port.output;
 
 import io.message.message.domain.interfaces.EntityAble;
 import io.message.message.domain.model.MechanicalSignal;
-import java.util.concurrent.ExecutionException;
 import reactor.core.publisher.Mono;
 
 public interface SignalOutput<T extends MechanicalSignal> {
 
-    Mono<T> save(EntityAble<T> messageAble) throws ExecutionException, InterruptedException;
+    Mono<T> save(EntityAble<T> messageAble);
 }
