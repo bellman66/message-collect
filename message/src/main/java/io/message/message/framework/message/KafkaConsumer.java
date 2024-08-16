5bfc0d52c7840320b716660c41f1ca35110f2d2c
bellman66 <rndso15@naver.com>
2024-08-16 21:59:46 +0900

    에러 발생시 Pending 메세지 발행
    Feat: 에러 발생시 Pending 메세지 발행

--- message/src/main/java/io/message/message/framework/message/KafkaConsumer.java
+++ message/src/main/java/io/message/message/framework/message/KafkaConsumer.java
@@ -3,9 +3,12 @@ package io.message.message.framework.message;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.message.message.application.port.output.SearchOutput;
 import io.message.message.application.port.output.SignalOutput;
+import io.message.message.domain.enums.MessageStatus;
+import io.message.message.domain.message.PendingMessage;
 import io.message.message.domain.message.SignalMessage;
 import io.message.message.domain.model.MechanicalSignal;
 import io.message.message.domain.search.SignalSearch;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -15,6 +18,7 @@ import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Slf4j
 @Service
@@ -26,6 +30,7 @@ public class KafkaConsumer implements ApplicationRunner {
     private final SearchOutput<SignalSearch> searchOutput;
 
     private final ReactiveKafkaConsumerTemplate<String, ConsumerRecord<String, String>> recordReactiveKafkaConsumerTemplate;
+    private final KafkaProducer<PendingMessage> pendingMessageKafkaProducer;
 
     public Flux<?> consumePublishMessage() {
         return recordReactiveKafkaConsumerTemplate
@@ -38,16 +43,20 @@ public class KafkaConsumer implements ApplicationRunner {
                 )
                 .flatMap(consumerRecord -> Mono.just(objectMapper.convertValue(consumerRecord.value(), SignalMessage.class)))
                 .flatMap(signalMessage -> {
-                    try {
-                        Mono<SignalSearch> searchSaveMono = searchOutput.save(signalMessage);
-                        Mono<MechanicalSignal> signalSaveMono = signalOutput.save(signalMessage);
+                    Mono<SignalSearch> searchSaveMono = searchOutput.save(signalMessage);
+                    Mono<MechanicalSignal> signalSaveMono = signalOutput.save(signalMessage);
 
-                        return Mono.zip(searchSaveMono, signalSaveMono);
-                    } catch (Exception e) {
-                        return Mono.error(e);
-                    }
+                    return Mono.zip(searchSaveMono, signalSaveMono);
                 })
-                .doOnError(Throwable::printStackTrace);
+                .publishOn(Schedulers.boundedElastic())
+                .doOnError(throwable -> {
+                    pendingMessageKafkaProducer.pending(PendingMessage.builder()
+                                    .id(UUID.randomUUID().toString())
+                                    .status(MessageStatus.DRAFT)
+                                    .throwable(throwable)
+                                    .build())
+                            .subscribe();
+                });
     }
 
     @Override
