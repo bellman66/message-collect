b935b3add3874c1ac5354289c2441d17f7a61a26
bellman66 <rndso15@naver.com>
2024-09-01 19:36:24 +0900

    모듈리스 테스트 코드 추가
    Test: 모듈리스 테스트 코드 추가

--- message/src/test/java/io/message/message/MessageApplicationTests.java
+++ message/src/test/java/io/message/message/MessageApplicationTests.java
@@ -2,10 +2,22 @@ package io.message.message;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class MessageApplicationTests {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
+
+    @Test
+    void writeDocumentationSnippets() {
+        var modules = ApplicationModules.of(MessageApplication.class).verify();
+
+        new Documenter(modules)
+                .writeModulesAsPlantUml()
+                .writeIndividualModulesAsPlantUml();
+    }
 }
