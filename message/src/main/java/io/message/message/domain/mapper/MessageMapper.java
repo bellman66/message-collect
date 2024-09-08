ae5c759b0122cc230adcf4910a6932ee40e85d26
bellman66 <rndso15@naver.com>
2024-09-08 17:20:37 +0900

    Mapper내 Tsid Generator 추가
    Feat: Mapper내 Tsid Generator 추가

--- message/src/main/java/io/message/message/domain/mapper/MessageMapper.java
+++ message/src/main/java/io/message/message/domain/mapper/MessageMapper.java
@@ -1,7 +1,6 @@
 package io.message.message.domain.mapper;
 
 import io.message.message.adapter.web.data.request.MessageApiRequestGroup;
-import io.message.message.domain.message.PendingMessage;
 import io.message.message.domain.message.SignalMessage;
 import io.message.message.global.base.MapperExtension;
 import org.mapstruct.Mapper;
@@ -13,8 +12,6 @@ public interface MessageMapper extends MapperExtension {
 
     MessageMapper INSTANCE = Mappers.getMapper(MessageMapper.class);
 
-    @Mapping(target = "id", expression = GENERATE_ID_BY_UUID)
+    @Mapping(target = "id", expression = GENERATE_ID_BY_TSID)
     SignalMessage toMessage(MessageApiRequestGroup.CreateApiRequest request);
-
-    PendingMessage toPendingMessage(SignalMessage message, Throwable throwable);
 }
--- message/src/main/java/io/message/message/global/base/MapperExtension.java
+++ message/src/main/java/io/message/message/global/base/MapperExtension.java
@@ -2,5 +2,5 @@ package io.message.message.global.base;
 
 public interface MapperExtension {
 
-    String GENERATE_ID_BY_UUID = "java(java.util.UUID.randomUUID().toString())";
+    String GENERATE_ID_BY_TSID = "java(io.hypersistence.utils.hibernate.id.Tsid.FactorySupplier.INSTANCE.get().generate().toString())";
 }
