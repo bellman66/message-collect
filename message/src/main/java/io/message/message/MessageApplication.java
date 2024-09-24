855fa48f6e018309c47b541f987e384b0a36fa74
bellman66 <rndso15@naver.com>
2024-09-24 09:58:15 +0900

    Vertx Deploy 이전
    Feat: Vertx Deploy 이전

--- message/src/main/java/io/message/message/MessageApplication.java
+++ message/src/main/java/io/message/message/MessageApplication.java
@@ -1,5 +1,8 @@
 package io.message.message;
 
+import io.message.message.adapter.web.controller.MessageVerticle;
+import io.vertx.core.Vertx;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +11,12 @@ public class MessageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MessageApplication.class, args);
+    }
+
+    @PostConstruct
+    public void deployVerticle() {
+        Vertx vertx = Vertx.vertx();
+
+        vertx.deployVerticle(new MessageVerticle());
     }
 }
