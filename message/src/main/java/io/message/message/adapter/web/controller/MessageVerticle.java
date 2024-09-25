5871ef2a251e64749789f94333c8475269b1a123
bellman66 <rndso15@naver.com>
2024-09-25 10:58:31 +0900

    Sample 비즈니스 로직 추가
    Feat: Sample 비즈니스 로직 추가

--- message/src/main/java/io/message/message/adapter/web/controller/MessageVerticle.java
+++ message/src/main/java/io/message/message/adapter/web/controller/MessageVerticle.java
@@ -1,13 +1,22 @@
 package io.message.message.adapter.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.message.message.adapter.web.data.request.MessageApiRequestGroup;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class MessageVerticle extends AbstractVerticle {
+
+    private ObjectMapper objectMapper;
+
+    private MessageController messageController;
 
     @Override
     public void start() {
@@ -15,10 +24,24 @@ public class MessageVerticle extends AbstractVerticle {
         router.post("/message/publish")
                 .handler(routingContext -> routingContext.response().end("Message published"));
 
-        vertx.createHttpServer().requestHandler(req -> {
-            req.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x!");
-        }).listen(58001);
+        vertx.createHttpServer().requestHandler(req -> req.body()
+                .map(buffer -> {
+                    try {
+                        return objectMapper.readValue(buffer.getBytes(), MessageApiRequestGroup.CreateApiRequest.class);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .onComplete(answer -> {
+                    if (answer.succeeded()) {
+                        req.response()
+                                .putHeader("content-type", MediaType.APPLICATION_JSON_VALUE)
+                                .end(messageController.publishMessage(answer.result()).toString());
+                    } else {
+                        req.response()
+                                .setStatusCode(400)
+                                .end();
+                    }
+                })).listen(58001);
     }
 }
